db0a637465b7bcfccd9e0385e90e9cd471be9bde
pulseofmalstrem <lispandhaskell@gmail.com>
2016-10-04 11:47:20 +0300

    1

--- 2.4.py
+++ 2.4.py
@@ -1,6 +1,7 @@
 __author__ = 'student'
 A = list(map(int, input().split()))
 kol = A.count(A[1])
+ch = A[1]
 for i in range(len(A)):
     if A.count(A[i])>kol:
         kol = A.count(A[i])
--- 7.py
+++ 7.py
@@ -3,7 +3,6 @@ A = list(map(int, input().split()))
 n = len(A)
 for i in range(n-1):
     if (A[i]==2):
-        if A[i+1]!=2:
-            A[i] = 0
-            n -=1
+        A[i] = 0
+        n -=1
 print(sum(A)//n)

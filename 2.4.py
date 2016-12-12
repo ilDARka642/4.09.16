8d6f740ca4067ab7d519e938328ba3bcb9313d4e
ilDARka642 <gab.ild@yandex.ru>
2016-12-13 02:06:57 +0500

    Delete 2.4.py

--- 2.4.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'student'
-A = list(map(int, input().split()))
-kol = A.count(A[1])
-ch = A[1]
-for i in range(len(A)):
-    if A.count(A[i])>kol:
-        kol = A.count(A[i])
-        ch = A[i]
-print(ch)
